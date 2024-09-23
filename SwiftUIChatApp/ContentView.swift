d82c4fe2f7a5eda39aaf2453c13a6082c84c0df5
Riffath RHM <mohemmad.riffath@elegantmedia.lk>
2024-09-23 21:51:43 +0530

    Initial Commit

---
+++ SwiftUIChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIChatApp 
+// Created by Mohamed Riffath on  9/23/24.
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIChatApp/SwiftUIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatAppApp.swift
+//  SwiftUIChatApp 
+// Created by Mohamed Riffath on  9/23/24.
+
+
+
+import SwiftUI
+
+@main
+struct SwiftUIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
