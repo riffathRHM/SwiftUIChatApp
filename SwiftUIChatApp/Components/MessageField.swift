8790157d3b7d72b3ec664c687018498e2f3f9071
Riffath RHM <mohemmad.riffath@elegantmedia.lk>
2024-10-06 21:41:45 +0530

    craeta a custom text field and messageBubble.

---
+++ SwiftUIChatApp/Components/MessageField.swift
@@ -0,0 +1,18 @@
+//
+//  MessageField.swift
+//  SwiftUIChatApp 
+// Created by Mohamed Riffath on  9/29/24.
+
+
+
+import SwiftUI
+
+struct MessageField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageField()
+}
